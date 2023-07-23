81e3901e38fc7722e79d899cfbb562ac55726f7f
nda0255 <106548980+nda0255@users.noreply.github.com>
2023-07-23 13:21:54 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xd701101fd4be80985f73545be52e64c2294ce42c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
